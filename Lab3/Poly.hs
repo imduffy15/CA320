4db8690a2942088d2e030ff18afe34e8f82a1e31
Ian Duffy <ian@ianduffy.ie>
2013-11-05 14:55:23 +0000

    Fin lab 3

--- Lab3/Poly.hs
+++ Lab3/Poly.hs
@@ -4,3 +4,7 @@ sumPoly :: Poly -> Poly -> Poly
 sumPoly xs [] = xs
 sumPoly [] ys = ys
 sumPoly (x:xs) (y:ys) = (x+y) : sumPoly xs ys
+
+evalPoly :: Int -> [Int] -> Int
+evalPoly x [] = 0
+evalPoly a (x:xs) = x + a * (evalPoly a xs)
